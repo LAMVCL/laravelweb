7327de81a5ad6bf1e05729ab9a13d235972bb2b2
LAMVCL <luismartinez311@outlook.es>
2020-01-25 00:10:59 -0300

    actualizaciones al footer + logo eliminado

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -42,22 +42,20 @@
     <div class="container-fluid text-center text-md-left">
       <div class="row">
         <div class="col-md-6 mt-md-0 mt-3">
-          <h5 class="text-uppercase font-weight-bold"> Footer text 1</h5>
-          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita sapiente sint, nulla, nihil
-            repudiandae commodi voluptatibus corrupti animi sequi aliquid magnam debitis, maxime quam recusandae
-            harum esse fugiat. Itaque, culpa?</p>
+          <h5 class="text-uppercase font-weight-bold"> Información</h5>
+          <p>Esta página fue creada con la finalidad de practicar laravel 6.</p>
         </div>
         <hr class="clearfix w-100 d-md-none pb-3">
         <div class="col-md-6 mb-md-0 mb-3">
-          <h5 class="text-uppercase font-weight-bold">Footer text 2</h5>
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio deserunt fuga perferendis modi earum
-            commodi aperiam temporibus quod nulla nesciunt aliquid debitis ullam omnis quos ipsam, aspernatur id
-            excepturi hic.</p>
+          <h5 class="text-uppercase font-weight-bold">Contacto y Redes</h5>
+          <a class="footerlink text-justify text-decoration-none text-white" href="https://github.com/LAMVCL">
+            <img class="imghub" src="{{URL::asset('img/logo-github.png')}}">
+          </a>
         </div>
       </div>
     </div>
     <div class="footer-copyright text-center py-3">© 2020 Copyright:
-        <a href="{{route('inicio')}}"> Luis Martínez</a>
+        <a class="" href="{{route('inicio')}}"> Luis Martínez</a>
     </div>
     </footer>
 
